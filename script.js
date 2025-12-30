62e2d1133407b6ff0e53cc4faf99f27743f3a005
Dipak <dipakxb@gmail.com>
2025-12-30 17:40:03 +0530

    Add slider completed basic MVP
    added slider to select size from 4 preset 16x16 ; 32x32 ; 64x64 ; 128x128 ;

--- script.js
+++ script.js
@@ -3,27 +3,51 @@ const canvas = document.createElement('div');
 container.appendChild(canvas);
 canvas.className = 'canvas';
 
-const initializeDiv = (length, type) => {
-    for(let n = 0; n < length; n++) {
+let pixelList = '';
+
+const initializeDiv = (size, type) => {
+    let length = Math.sqrt(size);
+    for(let n = 0; n < size; n++) {
         const element = document.createElement(type);
         canvas.appendChild(element);
         element.className = 'pixel';
     }
+    pixelList = document.querySelectorAll('.pixel')
+    pixelList.forEach(e => {
+        e.style.flexBasis = `${(1/length) * 100}%`;
+    })
 }
 
-initializeDiv(10000, 'div');
+const buttons = document.createElement('div');
+container.appendChild(buttons);
+buttons.className = 'buttons'
 
-//change color of div if mouse is pressed and hovered over a pixel
+const sliderLabel = document.createElement('label');
+
+const slider = document.createElement('input');
+buttons.appendChild(slider);
+const sliderAttribute = {
+    class: 'slider',
+    type: 'range',
+    name: 'pixel-slider',
+    min: '4',
+    max: '16',
+    step: '4'
+}
+for(let [className, attribute] of Object.entries(sliderAttribute)){
+    slider.setAttribute(className, attribute);
+}
+
+//change color of div if mouse is pressed and hovered over a <pixel></pixel>
 //change color on event (for mousedown & mouseover only)
 let isMousePressed = false;
 canvas.onmousedown = (e)=> {
     e.preventDefault()
-    isMousePressed = true
-    console.log('canvas down');
+    isMousePressed = true;
 };
 window.onmouseup = () => {
     isMousePressed = false;
-    console.log('up')    
+    isSliderPressed = false;
 }
 
 canvas.addEventListener('mouseover', e => {
@@ -35,9 +59,39 @@ canvas.addEventListener('mouseover', e => {
         }
         eventElement.onmousedown = ()=> {
             e.stopPropagation();
-            console.log('pixel mousedown')
             eventElement.style.backgroundColor = 'red';
         }
     };
 })
 
+
+// let isSliderPressed = false;
+// slider.onmousedown = () => {
+//     isSliderPressed = true;
+// }
+
+slider.addEventListener('change', (e)=> {
+    canvas.innerHTML = '';
+    const value = e.target.value;
+    if(value) {
+        value == 4 ? length = 16 : '';
+        value == 8 ? length = 32 : '';
+        value == 12 ? length = 64 : '';
+        value == 16 ? length = 100 : '';
+        initializeDiv(length * length, 'div');
+    }
+})
+
+slider.onmousedown = () => {
+    const changeEvent = new Event('change');
+    slider.dispatchEvent(changeEvent);
+        pixelList.forEach(e => {
+        e.style.border = '1px solid black';
+    })
+} 
+
+slider.onmouseup = () => {
+    pixelList.forEach(e => {
+        e.style.border = 'transparent';
+    })
+}
